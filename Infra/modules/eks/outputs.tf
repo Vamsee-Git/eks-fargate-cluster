2952298ddc991dcdf19b1ae9321cde5a51a89628
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-21 17:12:01 +0530

    Create outputs.tf

---
+++ Infra/modules/eks/outputs.tf
@@ -0,0 +1,19 @@
+output "eks_cluster_id" {
+  description = "ID of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "eks_cluster_endpoint" {
+  description = "Endpoint of the EKS cluster"
+  value       = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_arn" {
+  description = "ARN of the EKS cluster"
+  value       = module.eks.cluster_arn
+}
+
+output "eks_role_arn" {
+  description = "ARN of the EKS IAM role"
+  value       = aws_iam_role.eks_role.arn
+}
